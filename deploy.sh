6d8a131cf6cd12437d404932c96e995e646c034c
DESKTOP-S62A507\l'g <1762861715@qq.com>
2022-05-29 14:32:50 +0800

    脚本修改

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ git commit -m 'deploy'
 #git push -f git@gitee.com:lgpr/lgpr.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MrYZhou/vitepress.git main:gh-pages
+git push -f git@github.com:MrYZhou/vitepress.git master:gh-pages
 # git push -f git@gitee.com:lgpr/vitepress.git master:develop
 
 cd -
