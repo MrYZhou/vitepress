e160cc35c9909b2f8c7c8b344a3c741c66d08a88
DESKTOP-S62A507\l'g <1762861715@qq.com>
2022-05-29 14:52:42 +0800

    deploy

---
+++ assets/components_install.md.2b33a1c8.lean.js
@@ -0,0 +1,2 @@
+import{_ as t,c as a,o,b as e}from"./app.6d9129ed.js";const m='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"components/install.md"}',s={},n=e("div",{class:"language-"},[e("pre",null,[e("code",null,`hello world
+`)])],-1),l=[n];function r(c,_,d,i,p,u){return o(),a("div",null,l)}var h=t(s,[["render",r]]);export{m as __pageData,h as default};
