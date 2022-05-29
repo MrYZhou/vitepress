8e5a1315c8584488af381558e0abf7a1495d089e
DESKTOP-S62A507\l'g <1762861715@qq.com>
2022-05-29 13:13:07 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -17,13 +17,13 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:MrYZhou/MrYZhou.github.io.git master
+#git push -f git@github.com:MrYZhou/MrYZhou.github.io.git master
 
 #gitee
 #git push -f git@gitee.com:lgpr/lgpr.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:MrYZhou/vitepress.git main:gh-pages
 # git push -f git@gitee.com:lgpr/vitepress.git master:develop
 
 cd -
